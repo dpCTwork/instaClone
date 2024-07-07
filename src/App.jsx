82358caead9be2fb8dde32020974117c9f788412
David Park <davidpark.kh@gmail.com>
2024-07-07 11:21:30 -0400

    initialized tailwindcss (thought I did this before, but guess not). Changed pages/files names for simplicity. Added a new favicon to replace the default.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import { Button } from "@chakra-ui/react"
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home/Home"
+import Auth from "./pages/Auth/Auth"
 
 function App() {
 	return (
 		<>
-			<Button>hello world</Button>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/auth" element={<Auth />} />
+			</Routes>
 		</>
 	)
 }

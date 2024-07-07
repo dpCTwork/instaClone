29b7e1e62ada77e7c22ef4ce6c1810b3baf30e66
David Park <davidpark.kh@gmail.com>
2024-07-06 22:44:25 -0400

    initialized react app with vite, installed ChakraUI and set up ChakraProvider and the theme file.

---
+++ src/theme.js
@@ -0,0 +1,25 @@
+import { mode } from "@chakra-ui/theme-tools"
+
+// 1. import `extendTheme` function
+import { extendTheme } from "@chakra-ui/react"
+
+// 2. Add your color mode config
+const config = {
+	initialColorMode: "dark",
+	useSystemColorMode: false,
+}
+
+// 2a. My own customizations to the theme
+const styles = {
+	global: (props) => ({
+		body: {
+			bg: mode("gray.100", "#000")(props),
+			color: mode("gray.800", "whiteAlpha.900")(props),
+		},
+	}),
+}
+
+// 3. extend the theme
+const theme = extendTheme({ config, styles })
+
+export default theme

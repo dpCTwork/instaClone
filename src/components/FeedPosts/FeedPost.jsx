f5b630bc1176556864bcc12a7ae321a0f73f11d6
David Park <davidpark.kh@gmail.com>
2024-07-14 17:04:36 -0400

    finished main FeedPosts components and started the SuggestedUsers components

---
+++ src/components/FeedPosts/FeedPost.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+import { Box, Image } from "@chakra-ui/react"
+import PostFooter from "./PostFooter"
+import PostHeader from "./PostHeader"
+
+const FeedPost = ({ img, username, avatar }) => {
+	return (
+		<>
+			<PostHeader username={username} avatar={avatar} />
+			<Box my={2} overflow={"hidden"} borderRadius={4}>
+				<Image src={img} alt={username} />
+			</Box>
+			<PostFooter username={username} />
+		</>
+	)
+}
+
+export default FeedPost
---
+++ src/components/SuggestedUsers/SuggestedHeader.jsx
@@ -0,0 +1,5 @@
+const SuggestedHeader = () => {
+	return <div>SuggestedHeader</div>
+}
+
+export default SuggestedHeader

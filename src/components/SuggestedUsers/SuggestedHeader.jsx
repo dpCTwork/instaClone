85a544f70e7a2cef713240c43c87dc3fbdb3f0c8
David Park <davidpark.kh@gmail.com>
2024-07-14 17:29:49 -0400

    finished SuggestedUsers components.

--- src/components/SuggestedUsers/SuggestedHeader.jsx
+++ src/components/SuggestedUsers/SuggestedHeader.jsx
@@ -1,5 +1,31 @@
+import { Avatar, Link, Flex, Text } from "@chakra-ui/react"
+import { Link as RouterLink } from "react-router-dom"
+
 const SuggestedHeader = () => {
-	return <div>SuggestedHeader</div>
+	return (
+		<Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
+			<Flex alignItems={"center"} gap={2}>
+				<Avatar
+					name="As a Programmer"
+					size={"lg"}
+					src="/profilepic.png"
+				/>
+				<Text fontSize={12} fontWeight={"bold"}>
+					asaprogrammer_
+				</Text>
+				<Link
+					as={RouterLink}
+					to={"/auth"}
+					fontSize={14}
+					fontWeight={"medium"}
+					color={"blue.400"}
+					style={{ textDecoration: "none" }}
+					cursor={"pointer"}>
+					Log Out
+				</Link>
+			</Flex>
+		</Flex>
+	)
 }
 
 export default SuggestedHeader
--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,5 +1,54 @@
+import { Box, Flex, Link, Text, VStack } from "@chakra-ui/react"
+import SuggestedHeader from "./SuggestedHeader"
+import SuggestedUser from "./SuggestedUser"
+
 const SuggestedUsers = () => {
-	return <div>SuggestedUsers</div>
+	return (
+		<VStack py={8} px={6} gap={4}>
+			<SuggestedHeader />
+			<Flex
+				alignItems={"center"}
+				justifyContent={"space-between"}
+				w={"full"}>
+				<Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
+					Suggested for you
+				</Text>
+				<Text
+					fontSize={12}
+					fontWeight={"bold"}
+					_hover={{ color: "gray.400" }}
+					cursor={"pointer"}>
+					See All
+				</Text>
+			</Flex>
+			<SuggestedUser
+				name="Dan Abrahmov"
+				followers={1392}
+				avatar="https://bit.ly/dan-abramov"
+			/>
+			<SuggestedUser
+				name="Ryan Florence"
+				followers={567}
+				avatar="https://bit.ly/ryan-florence"
+			/>
+			<SuggestedUser
+				name="Christian Nwamba"
+				followers={759}
+				avatar="https://bit.ly/code-beast"
+			/>
+
+			<Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
+				&copy; 2024 Built by{" "}
+				<Link
+					href="https://github.com/dpCTwork"
+					target="_blank"
+					color="blue.500"
+					fontSize={14}>
+					David Park
+				</Link>
+			</Box>
+		</VStack>
+	)
 }
 
 export default SuggestedUsers

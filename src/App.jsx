9fa8364b51de7501082899c3b35c2f4a21898aa7
David Park <davidpark.kh@gmail.com>
2024-07-16 14:52:02 -0400

    added profile components - profileheader, profilepost, profileposts, profiletabs. Finished profileheader.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home/Home"
 import Auth from "./pages/Auth/Auth"
 import PageLayout from "./layouts/PageLayout/PageLayout"
+import Profile from "./pages/Profile/Profile"
 
 function App() {
 	return (
@@ -9,6 +10,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/auth" element={<Auth />} />
+				<Route path="/:username" element={<Profile />} />
 			</Routes>
 		</PageLayout>
 	)
